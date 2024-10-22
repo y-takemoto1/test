31bd8702027ba78d0d805dc214c3969519ed6d77
takemoto <y-takemoto@kts-it.com>
2024-10-22 16:23:34 +0900

    test

--- main_app/app.py
+++ main_app/app.py
@@ -1,60 +1,24 @@
-# coding:utf-8
-
-# 必要なパッケージのインポート
-import streamlit as st
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome import service as fs
-from selenium.webdriver import ChromeOptions
-from webdriver_manager.core.os_manager import ChromeType
+from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
 
-# タイトルを設定
-st.title("seleniumテストアプリ")
+# ChromeDriverのパスを取得
+service = ChromeService(ChromeDriverManager().install())
 
-# ボタンを作成(このボタンをアプリ上で押すと"if press_button:"より下の部分が実行される)
-press_button = st.button("スクレイピング開始")
+# Chromeブラウザを起動
+driver = webdriver.Chrome(service=service)
 
-if press_button:
-    # スクレイピングするwebサイトのURL
-    URL = "https://www.staff-q.co.jp/"
+driver.implicitly_wait(5)
+driver.get('https://mynavi-ms.jp/search/fukuoka/area-all/')
 
-    # ドライバのオプション
-    options = ChromeOptions()
+# クラス名で要素を取得
+job_cards = driver.find_elements(By.XPATH, '//div[@class="job-summary"]/div/h3/a[@target="_blank"]')
+table_cards = driver.find_elements(By.CLASS_NAME, 'job-summary-table')
 
+# 要素のテキストを表示
+print([t.text for t in job_cards])
+print([s.text for s in table_cards])
 
-    # option設定を追加（設定する理由はメモリの削減）
-    options.add_argument("--headless")
-    options.add_argument('--disable-gpu')
-    options.add_argument('--no-sandbox')
-    options.add_argument('--disable-dev-shm-usage')
-
-    # webdriver_managerによりドライバーをインストール
-    CHROMEDRIVER = ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()
-    service = fs.Service(CHROMEDRIVER)
-    driver = webdriver.Chrome(
-                              options=options,
-                              service=service
-                             )
-
-    # URLで指定したwebページを開く
-    driver.get(URL)
-    st.text(f'driver : {driver}')
-
-    # webページ上のタイトル画像を取得
-    img = driver.find_element(By.TAG_NAME, 'img')
-    src = img.get_attribute('src')
-    st.text(f'img : {img}')
-
-    # 取得した画像をカレントディレクトリに保存
-    with open(f"tmp_img.png", "wb") as f:
-        f.write(img.screenshot_as_png)
-
-    # 保存した画像をstreamlitアプリ上に表示
-    st.image("tmp_img.png")
-
-    # webページを閉じる
-    driver.close()
-
-    # スクレピン完了したことをstreamlitアプリ上に表示する
-    st.write("スクレイピング完了!!!")
+# ブラウザを閉じる
+driver.quit()
