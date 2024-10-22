11d392716b4febe1f919093414d852f68361b6c7
takemoto <y-takemoto@kts-it.com>
2024-10-22 17:44:55 +0900

    test2

--- main_app/app.py
+++ main_app/app.py
@@ -7,10 +7,9 @@ import streamlit as st
 import os
 
 st.text('ルート確認１')
-URL_input2 = st.text_input('URL')
 
 # スクレイピングしたいURL（例: 福岡の求人）
-url = URL_input2
+url = 'https://mynavi-ms.jp/'
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36 Edg/122.0.0.0'
