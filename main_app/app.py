4799625576f3bfd30a39c9233189bc4d03b9b850
takemoto <y-takemoto@kts-it.com>
2024-10-22 16:37:00 +0900

    test

--- main_app/app.py
+++ main_app/app.py
@@ -24,7 +24,7 @@ if press_button:
 
 
     # option設定を追加（設定する理由はメモリの削減）
-    #options.add_argument("--headless")
+    options.add_argument("--headless")
     options.add_argument('--disable-gpu')
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
