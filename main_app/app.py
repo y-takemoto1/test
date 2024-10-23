c5c108b2771c2b5897e1c71d1baee4630f08eb08
takemoto <y-takemoto@kts-it.com>
2024-10-23 16:05:06 +0900

    teste

--- main_app/app.py
+++ main_app/app.py
@@ -11,11 +11,13 @@ from selenium.webdriver.common.by import By
 import time
 import re
 import openpyxl
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 # タイトルを設定
 st.title("seleniumテストアプリ")
 
-url = st.text_input('URL入力')
+url = 'https://jp.indeed.com/jobs?q=%E6%AD%A3%E7%A4%BE%E5%93%A1&l=%E7%A6%8F%E5%B2%A1%E7%9C%8C&from=searchOnDesktopSerp&vjk=d4bb56841be7498e'
 
 # 初期の値を表示するための空の場所を作成
 text_placeholder = st.empty()
@@ -50,6 +52,8 @@ if press_button:
 
     # URLで指定したwebページを開く
     driver.get(URL)
+    # ページが読み込まれるのを待つ
+    time.sleep(5)
 
     if url:
         # ステータスコードをチェック
@@ -58,13 +62,13 @@ if press_button:
         j = 1  # ページ数
         while c <= 10:
             print(f'Processing page {j}...')
-            job_cards = driver.find_elements('div', class_='job-summary__header')
-            table_cards = driver.find_elements('table', class_='job-summary-table')
+            # 求人情報の取得
+            job_cards = driver.find_elements(By.CLASS_NAME, "slider_item css-17bghu4 eu4oa1w0")
 
             if not job_cards:
                 print('求人が見つかりませんでした。')
                 break
-            for job, job_table in zip(job_cards, table_cards):
+            for job in job_cards:
                 print('testcount:', c)
 
                 if st.session_state.stop:
@@ -77,27 +81,12 @@ if press_button:
                 text_placeholder.text(f'読込数：{c}')
 
                 # 求人タイトルを取得
-                title = job.find('a', target='_blank').text.strip()
+                title = job.find_element(By.TAG_NAME, 'title').text.strip()
                 print('求人タイトル:', title)
                 
                 # 会社名を取得
-                company = job.find('p', class_='job-summary__name').text.strip()
+                company = job.find_element(By.ID, 'company-name').text.strip()
                 print('会社名:', company)
-
-                # テーブルデータを取得（例として最初のテーブルを取得）
-                rows = job_table.find_all('tr')
-
-                # テーブルデータをリストに格納
-                table_data = []
-                for row in rows:
-                    cols = row.find_all('td')
-                    cols = [col.get_text(strip=True) for col in cols]
-                    if cols:  # 空でない場合のみ追加
-                        table_data.append(cols)
-
-                # 1つのセルにデータを書き込むための文字列を作成
-                cell_data = '\n'.join(['\t'.join(row) for row in table_data])
-
 
                 # 詳細ページのリンクを取得
                 job_link = 'https://mynavi-ms.jp/' + job.find('a')['href']
@@ -106,7 +95,15 @@ if press_button:
                 time.sleep(10)
                 # 詳細ページにリクエストを送信
                 job_response = driver.get(job_link)
-    
+
+                # 勤務地を取得
+                location = job.find_element(By.ID, 'jobLocationText').text.strip()
+                print('勤務地：', location)
+
+                # 仕事内容
+                job_description = job.find_element(By.TAG_NAME, 'JobDescription')
+                print('仕事内容：', job_description)
+
                 # 電話番号を含む要素を取得
                 text_elements = driver.find_elements(By.CSS_SELECTOR, '.phone')  # 適切なセレクタを使用
                 all_text = ' '.join([element.text for element in text_elements])
@@ -119,17 +116,23 @@ if press_button:
                     ws.cell(row=c, column=1, value=title)
                     ws.cell(row=c, column=2, value=company)
                     ws.cell(row=c, column=3, value=', '.join(phone_numbers) if phone_numbers else 'なし')
-                    ws.cell(row=c, column=4, value=cell_data)
+                    ws.cell(row=c, column=4, value=location)
+                    ws.cell(row=c, column=5, value=job_description)
                     c += 1
                 else:
                     print('電話番号は見つかりませんでした。')
-                
+                driver.back()
                 print('-' * 40)
+                WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.CLASS_NAME, "jobTitle css-1psdjh5 eu4oa1w0"))
+                )
+                time.sleep(1) 
+
             # 次のページを探す
-            next_page = driver.find('a', class_='search-pagenation__next')
+            next_page = driver.find_element(By.TAG_NAME, 'Next Page')
             if next_page and 'href' in next_page.attrs:
                 next_url = 'https://mynavi-ms.jp' + next_page['href']
-                response = requests.get(next_url, headers=headers)
+                response = driver.get(next_url)
                 time.sleep(5)  # サーバーへの負荷を避けるためにスリープ
                 j += 1
             else:
@@ -139,7 +142,7 @@ if press_button:
             st.text('完了しました。')
             #st.text("C:/aggregate/に保存されました。")
     else:
-        print('リクエストが失敗しました。ステータスコード:', response.status_code)
+        print('リクエストが失敗しました。ステータスコード')
 
 
     # webページを閉じる
@@ -147,3 +150,6 @@ if press_button:
 
     # スクレピン完了したことをstreamlitアプリ上に表示する
     st.write("スクレイピング完了!!!")
+
+if st.button("中断", disabled=st.session_state.processing):
+    st.session_state.stop = True
