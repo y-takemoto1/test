4b60b754ea90f24b51462c877c14fc40b0dc6c22
takemoto <y-takemoto@kts-it.com>
2024-10-23 11:55:34 +0900

    test

--- main_app/app.py
+++ main_app/app.py
@@ -23,7 +23,6 @@ if press_button:
     options = ChromeOptions()
 
     # option設定を追加（設定する理由はメモリの削減）
-    options.add_argument("--headless")
     options.add_argument('--disable-gpu')
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
