0cc1be27a1fc4054f53a9f26d7e8b7f592e4d97b
takemoto <y-takemoto@kts-it.com>
2024-10-23 13:38:44 +0900

    teste

--- main_app/app.py
+++ main_app/app.py
@@ -17,7 +17,7 @@ press_button = st.button("スクレイピング開始")
 
 if press_button:
     # スクレイピングするwebサイトのURL
-    URL = "https://ohenziblog.com"
+    URL = "https://mynavi-ms.jp/search/fukuoka/area-all/"
 
     # ドライバのオプション
     options = ChromeOptions()
