753b91829d5e3db9ba0e66259b67b4e13cd872be
takemoto <y-takemoto@kts-it.com>
2024-10-30 14:17:55 +0900

    test

--- main_app/app.py
+++ main_app/app.py
@@ -8,6 +8,9 @@ from selenium.webdriver.chrome import service as fs
 from selenium.webdriver import ChromeOptions
 from webdriver_manager.core.os_manager import ChromeType
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+import time
+import openpyxl
 
 # タイトルを設定
 st.title("seleniumテストアプリ")
@@ -15,13 +18,16 @@ st.title("seleniumテストアプリ")
 # ボタンを作成(このボタンをアプリ上で押すと"if press_button:"より下の部分が実行される)
 press_button = st.button("スクレイピング開始")
 
+# エクセルを開く
+wb = openpyxl.Workbook()
+ws = wb.active
+
 if press_button:
     # スクレイピングするwebサイトのURL
-    URL = "https://ohenziblog.com"
+    URL = 'https://jp.indeed.com/jobs?q=%E6%AD%A3%E7%A4%BE%E5%93%A1&l=%E7%A6%8F%E5%B2%A1%E7%9C%8C&from=searchOnDesktopSerp&vjk=d4bb56841be7498e'
 
     # ドライバのオプション
     options = ChromeOptions()
-
     # option設定を追加（設定する理由はメモリの削減）
     options.add_argument("--headless")
     options.add_argument('--disable-gpu')
@@ -38,20 +44,125 @@ if press_button:
 
     # URLで指定したwebページを開く
     driver.get(URL)
+    # ページが読み込まれるまで待機
+    wait = WebDriverWait(driver, 10)
+    time.sleep(10)
+    c = 1
+    j = 1
 
-    # webページ上のタイトル画像を取得
-    img = driver.find_element(By.TAG_NAME, 'img')
-    src = img.get_attribute('src')
+    # 1ページにつき15件あるため15件単位取得される(例：10にすると150件取得可能)
+    #　 　　　　↓
+    while j <= 10:
+        print('COUNT:', c)
+        time.sleep(1)
+        while True:
+            try:
+                links = wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'job_seen_beacon')))
+                break
+            except Exception as e:
+                print("求人情報の取得に失敗:", e)
 
-    # 取得した画像をカレントディレクトリに保存
-    with open(f"tmp_img.png", "wb") as f:
-        f.write(img.screenshot_as_png)
+        for link in links:
+            # リンクをクリックする前に、再度要素を取得
+            try:
+                wait.until(EC.element_to_be_clickable(link)).click()
+            except Exception as e:
+                print("リンクのクリックに失敗:", e)
+                continue  # 失敗した場合は次のリンクに進む
 
-    # 保存した画像をstreamlitアプリ上に表示
-    st.image("tmp_img.png")
+            # 詳細ページが読み込まれるまで待機
+            wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'jobsearch-HeaderContainer')))
+            time.sleep(10)
 
-    # webページを閉じる
-    driver.close()
+            # 初期値を設定
+            title = location = company = tel = desired_element = "該当無し"
 
-    # スクレピン完了したことをstreamlitアプリ上に表示する
-    st.write("スクレイピング完了!!!")
+            # 特定の要素を取得
+            try:
+                custom_data_element = driver.find_elements(By.CLASS_NAME, 'css-1jkk9vb')
+                if custom_data_element:
+                    desired_element = custom_data_element[0].text
+                    print('仕事内容：', desired_element)
+                print('-' * 80)
+
+                try:
+                    title = driver.find_element(By.CLASS_NAME, 'jobsearch-JobInfoHeader-title').text
+                    print('title:', title)
+                except Exception as e:
+                    print('タイトルの取得に失敗:')            
+                print('-' * 80)
+
+                try:
+                    location = driver.find_element(By.CLASS_NAME, 'css-scnhev').text
+                    print('location:', location)
+                except Exception as e:
+                    print('勤務地の取得に失敗:')
+                print('-' * 80)
+
+                try:
+                    company = driver.find_element(By.CLASS_NAME, 'css-1f8zkg3').text
+                    print('company:', company)
+                except Exception as e:
+                    print('会社名の取得に失敗:')
+                print('-' * 80)
+
+                # 特定の文字列を持つ要素を検索
+                try:
+                    parent_element = driver.find_element(By.XPATH, "//div[text()='代表電話番号']")
+                    child_elements = parent_element.find_elements(By.XPATH, "../*")
+                    tel = child_elements[1].text if len(child_elements) > 1 else "該当無し"
+                    print('電話番号：', child_elements[1].text if len(child_elements) > 1 else "該当なし")
+                except Exception as e:
+                    print('電話番号の取得に失敗:')
+                print('-' *80)
+            except Exception as e:
+                print('詳細情報の取得に失敗:')
+                print('-' *80)
+            
+            ws.cell(row=c, column=1, value=title)
+            ws.cell(row=c, column=2, value=location)
+            ws.cell(row=c, column=3, value=company)
+            ws.cell(row=c, column=4, value=tel)
+            ws.cell(row=c, column=5, value=desired_element)
+            
+            c += 1
+            
+            # 詳細ページから戻る
+            #driver.back()
+            wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'job_seen_beacon')))
+            time.sleep(10)
+        # スクロール
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(2)
+        # 次のページを探す
+        try:
+            next_page = wait.until(EC.presence_of_element_located((By.XPATH, "//a[@aria-label='Next Page']")))
+            print('NEXT:', next_page.text)
+            if next_page:
+                next_page.click()
+        except Exception as e:
+            print('次のページの取得に失敗:')
+            break  # 次のページが取得できなかったらループを終了
+        # ポップアップが表示されている場合は閉じる
+        try:
+            # 一定時間待機してからポップアップをチェック
+            wait.until(EC.presence_of_element_located((By.XPATH, "//button[contains(@class, 'css-yi9ndv')]")))
+            close_button = driver.find_element(By.XPATH, "//button[contains(@class, 'css-yi9ndv')]")
+            time.sleep(10)
+            if close_button.is_displayed():  # ボタンが表示されているか確認
+                close_button.click()
+                print('ポップアップを閉じました。')
+        except Exception as e:
+            print('ポップアップ無し') 
+        j += 1
+
+wb.save("INDEED.xlsx")
+wb.close()
+
+time.sleep(10)
+
+# webページを閉じる
+driver.close()
+
+# スクレピン完了したことをstreamlitアプリ上に表示する
+st.write("スクレイピング完了!!!")
